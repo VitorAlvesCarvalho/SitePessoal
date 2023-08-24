247cc4fc11b50a16a24f4987b23f6d44e8347ef0
VitorAlvesCarvalho <vitor.alves2402@gmail.com>
2023-08-24 01:09:40 -0300

    chore: componente de titulo da seção

---
+++ src/components/Sections/TitleSection.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="title">
+    <div class="title__divider"></div>
+
+    <div>
+      <span>{{ text }}</span>
+      <span class="title__mark">{{ textComplement }}</span>
+    </div>
+
+    <div class="title__divider"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TitleSection",
+
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+
+    textComplement: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Poppins";
+  font-size: 36px;
+  font-weight: 700;
+  color: $white;
+  text-transform: uppercase;
+
+  &__mark {
+    background-color: $primary;
+    border-radius: 6px;
+    padding: 0 $spacing-2;
+    margin-left: $spacing-4;
+  }
+
+  &__divider {
+    margin: 0 $spacing-4;
+    border-radius: 4px;
+    width: 40px;
+    height: 2px;
+    background-color: $yellow;
+  }
+}
+</style>
